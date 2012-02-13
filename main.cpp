4ca8dc4883b20d558b49fb652309c3d65a57d35a
Dubourg Theo <tdubourg@if219-12.insa-lyon.fr>
2012-02-13 17:49:07 +0100

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 /* Includes non systemes ----------------------------------------------------------------------------- */
 #include "Heure.h"
@@ -39,17 +40,34 @@ int main(int argc, char** argv) {
     else 
     {
         //Code de la mère
+        
+        //-----------------------------------------initialisation---------------------------------
+        
+        //masquages des signaux
         struct sigaction action;
         action.sa_handler = SIG_IGN;
         action.sa_flags = 0;
         sigaction (SIGINT, &action, NULL);
         sigaction (SIGUSR1, &action, NULL);
         sigaction (SIGUSR2, &action, NULL);
-                
+        
+        //Création des canaux
+        mode_t mode;
+        char *chemin = "key_entree";
+        mode = S_IRUSR;
+        mkfifo (chemin, mode);  //Canal reliant Keyboard et Entree
+        char *chemin = "key_sortie";
+        mkfifo (chemin, mode);  //Canal reliant Keyboard et Sortie
+        
+        
+        
+        //---------------------------------------------Moteur-------------------------------------------
         int st = -1;
         do {
             waitpid(noKeyboard, &st, 0);
         } while(st != 0);
+        
+        //----------------------------------------------Destruction-------------------------------------
         
     }
     
