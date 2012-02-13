19549a50ae0e9675295ea9e27a1d00a66719a509
Dubourg Theo <tdubourg@if219-12.insa-lyon.fr>
2012-02-13 17:14:29 +0100

    modified:   dist/Debug/GNU-Linux-x86/tp_multitache 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,20 @@
  * Created on 13 février 2012, 15:08
  */
 
+/* Includes systemes ----------------------------------------------------------------------------- */
+
 #include <cstdlib>
+#include <time.h>
+#include <unistd.h> 
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/* Includes non systemes ----------------------------------------------------------------------------- */
 #include "Heure.h"
 #include "Menu.h"
 #include "Outils.h"
-#include <time.h>
-#include <unistd.h>
+
 
 using namespace std;
 
@@ -20,7 +28,23 @@ using namespace std;
 int main(int argc, char** argv) {
 
     InitialiserApplication(XTERM);
-    sleep(10);
+    
+    pid_t noKeyboard;
+    
+    if ((noKeyboard = fork()) == 0)
+    {
+        //Code du fils Keyboard
+        
+    }
+    else 
+    {
+        //Code de la mère
+        int st = -1;
+        do {
+            waitpid(noKeyboard, &st, 0);
+        } while(st != 0);
+       // sigaction (SIGINT,)
+    }
     
     TerminerApplication();
     return 0;
