b97a1583f7543b695c236a2ee30fd10481c5ec3e
Fhyren <fhyren@fhyren-G74Sx.(none)>
2012-02-27 11:39:07 +0100

    avancement des créations de la maman

--- main.cpp
+++ main.cpp
@@ -14,29 +14,45 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/shm.h>
 
 /* Includes non systemes ----------------------------------------------------------------------------- */
+
 #include "Heure.h"
 #include "Menu.h"
 #include "Outils.h"
 #include "keyboard.h"
 
+#define CLEF_COMPTEUR 11111
+#define CLEF_REQUETES 22222
 
 using namespace std;
 
-/*
- * 
- */
+
 int main(int argc, char** argv) {
 
     InitialiserApplication(XTERM);
     
     pid_t noKeyboard;
+    pid_t noEntrees;
+    pid_t noSortie;
+    int shmIdCompteur;
+    int shmIdRequetes;
+    void* shmPtCompteur;
+    void* shmPtRequetes;
     
     if ((noKeyboard = fork()) == 0)
     {
         //Code du fils Keyboard
         keyboard();
+    }
+    if ((noEntrees = fork()) == 0)
+    {
+        //Code des fils Entrées
+    }
+    if ((noSortie = fork()) == 0)
+    {
+        //Code du fils Sortie
     }
     else 
     {
@@ -54,13 +70,30 @@ int main(int argc, char** argv) {
         
         //Création des canaux
         mode_t mode;
-        char *chemin = "key_entree";
+        const char *chemin = "key_entree";
         mode = S_IRUSR;
         mkfifo (chemin, mode);  //Canal reliant Keyboard et Entree
-        delete chemin;
         chemin = "key_sortie";
         mkfifo (chemin, mode);  //Canal reliant Keyboard et Sortie
         
+        //Création des mémoires partagées
+        if ((shmIdCompteur = shmget (CLEF_COMPTEUR, sizeof(int), 0666 | IPC_CREAT)) < 0)
+        {
+            exit(1);
+        }
+        if ((shmIdRequetes = shmget (CLEF_REQUETES, sizeof(string), 0666 | IPC_CREAT)) < 0)//Me souvient plus de quoi on mettait dans celui là... @TODO
+        {
+            exit(1);
+        }
+        
+        if ((shmPtCompteur = shmat (shmIdCompteur, NULL, 0)) == (void*)-1)
+        {
+            exit(1);
+        }
+        if ((shmPtRequetes = shmat (shmIdRequetes, NULL, 0)) == (void*)-1)
+        {
+            exit(1);
+        }
         
         
         //---------------------------------------------Moteur-------------------------------------------
@@ -71,6 +104,22 @@ int main(int argc, char** argv) {
         
         //----------------------------------------------Destruction-------------------------------------
         
+        //Envoi du signal de fin d'application aux taches filles
+        kill (0, SIGUSR1);
+        
+        //Attente de la fin de toutes les taches filles pour detruire correctement les canaux et sémaphores
+        //@TODO
+        
+        
+        //Destruction des canaux, il est necessaire d'attendre qu'il n'y ai plus de lecteurs ni d'écrivains. 
+        chemin = "key_entree";
+        unlink(chemin);
+        chemin = "key_sortie";
+        unlink(chemin);
+        
+        //Destruction des mémoires partagées 
+        shmdt(shmPtCompteur);
+        shmdt(shmPtRequetes);
     }
     
     TerminerApplication();
--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=multit
+PROJECTNAME=TP-MultiTask
 
 # Active Configuration
 DEFAULTCONF=Debug
